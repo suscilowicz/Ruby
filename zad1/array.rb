15660d6eb9ccf00a72d5b6d74f622091922d2228
suscilowicz <s.uscilowicz@gmail.com>
2015-11-13 18:02:26 +0100

    update

--- zad1/array.rb
+++ zad1/array.rb
@@ -9,3 +9,35 @@ puts array[0, 2] # wyswietla elementy z zakresu 0-2 bez ostatniego
 n = 3 # definiujemy zmienna n i przypisujemy 3
 puts array.take(n) # wyswietla pierwsze n elementow
 puts array.drop(n) # wyswietla wszystko procz pierwszych n elementow
+
+puts array.length # wyswietla ilosc elementow
+puts array.size # jak wyzej
+puts array.count #  jak wyzej
+
+puts array.empty? # sprawdza czy tablica jest pusta
+puts array.include?(8 - 1) # sprawdza czy tablica posiada podany element (true)
+puts array.include?('piec') # jak wyzej (false)
+
+array.push('ostatni') # dodanie elemntu na koniec tablicy
+puts array
+array << ('jednak ten jest ostatni')
+puts array
+array.unshift(0) # dodanie elementu na poczatek tablicy
+puts array
+array.insert(2, 2) # dodanie elementu na pozycji 2
+puts array
+array.insert(0, 1, 4, 4, 'szesc', nil) # mozna dodac takze pare elementow
+puts array
+
+array.pop # usuniecie ostatniego elementu
+puts array
+array.shift # usuniecie pierwszego elementu
+puts array
+array.delete_at(1) # usuniecie elementu o indexie 1
+puts array
+array.delete('ostatni') # usuniecie elementu o nazwie zawartej w nawiasie
+puts array
+array.compact! # usuniecie pustych elementow z tablicy
+puts array
+array.uniq! # usuniecie elementow, ktore sie powtarzaja
+puts array
--- zad1/ruby.md
+++ zad1/ruby.md
@@ -16,7 +16,31 @@ n = 3 # definiujemy zmienna n i przypisujemy 3
 puts array.take(n) # wyswietla pierwsze n elementow
 puts array.drop(n) # wyswietla wszystko procz pierwszych n elementow
 ```
+Uzyskanie informacji o tablicy
+```ruby
+puts array.length # wyswietla ilosc elementow
+puts array.size # jak wyzej
+puts array.count #  jak wyzej
 
+puts array.empty? # sprawdza czy tablica jest pusta
+puts array.include?(8 - 1) # sprawdza czy tablica posiada podany element (true)
+puts array.include?('piec') # sprawdza czy tablica posiada podany element (false)
+```
+Dodawanie i usuwanie elementow
+```ruby
+array.push('ostatni') # dodanie elemntu na koniec tablicy
+array << ('jednak ten jest ostatni') # tak jak wyzej
+array.unshift(0) # dodanie elementu na poczatek tablicy
+array.insert(2, 2) # dodanie elementu na pozycji 2
+array.insert(0, 1, 4, 4, 'szesc', nil) # mozna dodac takze pare elementow
+
+array.pop # usuniecie ostatniego elementu
+array.shift # usuniecie pierwszego elementu
+array.delete_at(1) # usuniecie elementu o indexie 1
+array.delete('ostatni') # usuniecie elementu o nazwie zawartej w nawiasie
+array.compact! # usuniecie pustych elementow z tablicy
+array.uniq! # usuniecie elementow, ktore sie powtarzaja
+```
 ### Enumerable
 Sie robi sie
 
