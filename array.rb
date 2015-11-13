a9d05abf98939bb6ebf56143b9c41dd24f2b11af
suscilowicz <s.uscilowicz@gmail.com>
2015-11-13 16:01:09 +0100

    nowa struktura repo i re-build zadania 1

--- array.rb
+++
@@ -1,4 +0,0 @@
-a = [1, 2, 3]
-puts a
-puts a
-puts a
---
+++ zad1/array.rb
@@ -0,0 +1,11 @@
+array = [1, 2, 3, 4, 5, 'szesc', 8 - 1]
+puts array # wyswietla tablice
+puts array[2] # wyswietla trzeci element tablicy
+puts array.at(2) # to co wyzej, inny zapis
+puts array[-2] # wyswietla przedostatni element tablicy
+puts array.first # wyswietla pierwszy element tablicy
+puts array.last # wyswietla ostatni element tablicy
+puts array[0, 2] # wyswietla elementy z zakresu 0-2 bez ostatniego
+n = 3 # definiujemy zmienna n i przypisujemy 3
+puts array.take(n) # wyswietla pierwsze n elementow
+puts array.drop(n) # wyswietla wszystko procz pierwszych n elementow
